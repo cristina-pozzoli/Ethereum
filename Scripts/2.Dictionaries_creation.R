0b6537ae3890add292e7a98391ef83dd5acca161
cristina-pozzoli <cristina.pozzoli@icloud.com>
2024-04-07 17:38:16 +0200

    Dictionaries

--- Scripts/2.Dictionaries_creation.R
+++ Scripts/2.Dictionaries_creation.R
@@ -9,14 +9,14 @@ contract_address_dict <- unique(df[, .(contract_address)])
 contract_address_dict[, id := seq_len(nrow(contract_address_dict))]
 
 from_address_dict <- unique(df[, .(from_address)])
-from_address_dict[, id := seq_len(nrow(from_address_dict))]
-
 to_address_dict <- unique(df[, .(to_address)])
-to_address_dict[, id := seq_len(nrow(to_address_dict))]
+
+combined_address_dict <- merge(from_address_dict, to_address_dict, by.x = "from_address", by.y = "to_address", all = TRUE)
+
+combined_address_dict[, id := seq_len(nrow(combined_address_dict))]
+
 
 # Save dictionaries as csv files
 write.csv(from_address_dict, "from_address_dict.csv", row.names = FALSE)
 
-write.csv(to_address_dict, "to_address_dict.csv", row.names = FALSE)
-
-write.csv(contract_address_dict, "contract_address_dict.csv", row.names = FALSE)
+write.csv(combined_address_dict, "combined_address_dict.csv", row.names = FALSE)
